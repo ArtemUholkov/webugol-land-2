0f4a0807e12e84288b612b8c12917645998ed61b
Artem <artem.ugolkov@ukr.net>
2024-02-01 09:39:04 +0200

    try

---
+++ js/script.js
@@ -0,0 +1,86 @@
+AOS.init();
+
+const burger = document.querySelector('#burger');
+const popup = document.querySelector('#popup');
+const popupSlide = document.querySelector('#popupSlide');
+const sliderItem = document.querySelectorAll('.our_requirements_content_item');
+const sliderPhoto = document.querySelector('.our_requirements_img');
+const body = document.body;
+const casualLogo = document.querySelector('.logo-white');
+const colorLogo = document.querySelector('.logo-color');
+
+burger.addEventListener('click', burgerHandler);
+popup.addEventListener('click', (e) => {
+  burgerHandler(e);
+});
+
+sliderItem.forEach((e) => {
+  e.addEventListener('click', () => {
+    setImage(e, sliderItem);
+  });
+});
+
+function burgerHandler(e) {
+  if (popup.classList.contains('open')) {
+    popup.classList.add('close');
+    body.classList.remove('noscroll');
+    popupSlide.classList.add('slideout');
+    casualLogo.classList.remove('logo-hide');
+    colorLogo.classList.add('logo-hide');
+    setTimeout(() => {
+      casualLogo.classList.remove('logo-hide');
+      colorLogo.classList.add('logo-hide');
+      // colorLogo.classList.remove('logo-hide');
+      // casualLogo.classList.add('logo-hide');
+    }, 270);
+    setTimeout(() => {
+      popup.classList.remove('close');
+      popup.classList.remove('open');
+      popupSlide.classList.remove('slideout');
+      popupSlide.classList.remove('slidein');
+    }, 270);
+  } else {
+    body.classList.add('noscroll');
+    popup.classList.add('open');
+    popupSlide.classList.add('slidein');
+    setTimeout(() => {
+      //   casualLogo.classList.remove('logo-hide');
+      // colorLogo.classList.add('logo-hide');
+      colorLogo.classList.remove('logo-hide');
+      casualLogo.classList.add('logo-hide');
+    }, 270);
+  }
+  burger.classList.toggle('active');
+}
+
+function scrollToNumbers() {
+  document.querySelector('#numbers').scrollIntoView(true);
+}
+function scrollToSolutions() {
+  document.querySelector('#solutions').scrollIntoView(true);
+}
+function scrollToProcess() {
+  document.querySelector('#process').scrollIntoView(true);
+}
+function scrollToReviews() {
+  document.querySelector('#reviews').scrollIntoView(true);
+}
+function scrollToTeam() {
+  document.querySelector('#team').scrollIntoView(true);
+}
+function scrollToSub() {
+  document.querySelector('#sub').scrollIntoView(true);
+}
+let copiedText = document.querySelectorAll('#trythis');
+copiedText.forEach((elem) => elem.addEventListener('click', () => copyText(elem)));
+console.log(colorLogo);
+
+
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 50) {
+    $('.header').addClass('active');
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $('.header').removeClass('active');
+  }
+});
