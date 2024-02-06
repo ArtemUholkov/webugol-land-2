28d6ea3f04ae854dd8f5161d539068f8729259e6
Artem <artem.ugolkov@ukr.net>
2024-02-06 02:46:06 +0200

    asd

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ data = {
       },
     },
   },
-  relationships: { list: { data: { type: 'list', id: 'id' } } },
+  relationships: { list: { data: { type: 'list', id: 'YADZga' } } },
 };
 
 console.log(data.attributes.profile.data.attributes);
@@ -128,7 +128,7 @@ var data_js = {
 };
 
 function js_onSuccess() {
-  window.location = 'step-2.html';
+  window.location = 'thankyou.html';
 }
 
 function js_onError(error) {
@@ -137,6 +137,50 @@ function js_onError(error) {
 }
 
 var sendButton = document.getElementById('js_send');
+
+[].forEach.call(document.querySelectorAll('.phone_numba'), function (input) {
+  let keyCode;
+  function mask(event) {
+    event.keyCode && (keyCode = event.keyCode);
+    let pos = this.selectionStart;
+    if (pos < 3) event.preventDefault();
+    let matrix = '+1 (___) ___-____',
+      i = 0,
+      def = matrix.replace(/\D/g, ''),
+      val = this.value.replace(/\D/g, ''),
+      newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+    i = newValue.indexOf('_');
+    if (i != -1) {
+      i < 5 && (i = 3);
+      newValue = newValue.slice(0, i);
+    }
+    let reg = matrix
+      .substr(0, this.value.length)
+      .replace(/_+/g, function (a) {
+        return '\\d{1,' + a.length + '}';
+      })
+      .replace(/[+()]/g, '\\$&');
+    reg = new RegExp('^' + reg + '$');
+    if (!reg.test(this.value) || this.value.length < 5 || (keyCode > 47 && keyCode < 58))
+      this.value = newValue;
+    if (event.type == 'blur' && this.value.length < 5) this.value = '';
+  }
+
+  input.addEventListener('input', mask, false);
+  input.addEventListener('focus', mask, false);
+  input.addEventListener('blur', mask, false);
+  input.addEventListener('keydown', mask, false);
+  input.addEventListener('mouseup', (event) => {
+    event.preventDefault();
+    if (input.value.length < 4) {
+      input.setSelectionRange(4, 4);
+    } else {
+      input.setSelectionRange(input.value.length, input.value.length);
+    }
+  });
+});
 
 function js_send() {
   sendButton.value = 'Sending…';
@@ -149,6 +193,13 @@ function js_send() {
       js_onError(request.response);
     }
   };
+  let radiores = document.querySelector('.hiden_radio');
+  let radios = document.querySelectorAll('.radio_but');
+  radios.forEach((item) => {
+    if (item.checked == true) {
+      radiores.value = item.value;
+    }
+  });
 
   var subject = document.querySelector('#' + form_id_js + " [name='Subject']").value;
   var message = document.querySelector('#' + form_id_js + " [name='text']").value;
@@ -158,6 +209,8 @@ function js_send() {
   var phone = document.querySelector('#' + form_id_js + " [name='extra_phone_number']").value;
   var about = document.querySelector('#' + form_id_js + " [name='extra_about']").value;
   var location = document.querySelector('#' + form_id_js + " [name='extra_target']").value;
+  var radres = document.querySelector('#' + form_id_js + " [name='extra_budget']").value;
+  var email = document.querySelector('#' + form_id_js + " [name='extra_email']").value;
 
   data_js['subject'] = subject;
   data_js['text'] = message;
@@ -167,17 +220,21 @@ function js_send() {
   data_js['extra_last_name'] = lastname;
   data_js['extra_about'] = about;
   data_js['extra_target'] = location;
+  data_js['extra_budget'] = radres;
+  data_js['extra_email'] = email;
 
   data.attributes.profile.data.attributes.first_name = name;
   data.attributes.profile.data.attributes.last_name = lastname;
   data.attributes.profile.data.attributes.phone_number = phone;
+  data.attributes.profile.data.attributes.email = email;
 
   const options = {
     method: 'POST',
     headers: { revision: '2023-10-15', 'content-type': 'application/json' },
     body: JSON.stringify({ data }),
   };
-  fetch('https://a.klaviyo.com/client/subscriptions/?company_id=id', options)
+  console.log(options);
+  fetch('https://a.klaviyo.com/client/subscriptions/?company_id=TbQjBA', options)
     .then((response) => response)
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
